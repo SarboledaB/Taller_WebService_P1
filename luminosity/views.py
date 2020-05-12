313b747016b01064a4ee40dc79c77b6ff1b1e15a
Sebastian Arboleda <sarboledab@eafit.edu.co>
2020-05-12 15:15:11 -0500

    Se agrega el proyecto

---
+++ luminosity/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Luminosity
+from .serializers import LuminositySerializer
+
+class LuminosityViewSet(viewsets.ModelViewSet):
+    queryset = Luminosity.objects.all().order_by('-created')
+    serializer_class = LuminositySerializer
